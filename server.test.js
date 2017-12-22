ca9468517d4bdda8c2d88e055dc3874ee5dff404
thierry saintmaxent <thierry.saintmaxent@decathlon.com>
2017-12-22 14:58:45 +0100

    continuous-integration

---
+++ server.test.js
@@ -0,0 +1,10 @@
+
+describe("a block of tests", function() {
+
+  test("Say hello name if name valid", function() {
+    // expect something
+    const result = "Hello THIERRY!";
+    expect(result).toBe("Hello THIERRY!");
+  });
+
+});
